2634dfb26ec992dcc6b718cda9d6146e4998547c
gareth muirhead <gsmuirhead@gmail.com>
2021-02-04 21:18:44 +0000

    saves counts to dates, cleanup

--- subreddit_count_tickers.py
+++ subreddit_count_tickers.py
@@ -35,6 +35,32 @@ def write_day(date_id, ticker_names, count_vals, out_file):
     date_grp.create_dataset(name='Count', data=count_vals)
     out.close()
 
+def get_pushshift_url(subreddit_name, time_point):
+    # time point is unix UTC
+    sid_url = ("https://api.pushshift.io/reddit/search/submission/?subreddit=" +
+    subreddit_name + "&sort=asc&sort_type=created_utc&after=" +
+    str(time_point) +
+    "&fields=id,created_utc,num_comments&size=1000")# + "&before="+str(time_point_2) + "&size=1000"
+    return(sid_url)
+
+def is_next_day(tp1,tp2):
+    return(datetime.fromtimestamp(tp2).date() > datetime.fromtimestamp(tp1).date())
+
+def is_current_day(tp1,tp2):
+    return(datetime.fromtimestamp(tp2).date() == datetime.fromtimestamp(tp1).date())
+
+def to_date_str(ts):
+   return(datetime.fromtimestamp(ts).strftime("%b_%d_%Y"))
+
+def to_date(ts):
+    return(datetime.date(ts))
+
+def to_time_srt(ts):
+    return(datetime.fromtimestamp(current_time).strftime(("%b-%d-%Y %H:%M")))
+    
+
+#import pdb; pdb.set_trace()
+
 ### MAIN ######################################################################
 
 # praw
@@ -46,7 +72,7 @@ reddit = praw.Reddit(
     password=prv_password)
 
 # params
-subreddit_name = 'stocks'
+subreddit_name = 'wallstreetbets'
 comment_limit = 1000
 upvotes = 0
 
@@ -54,75 +80,50 @@ upvotes = 0
 out_file = "out/out_tickers_" + subreddit_name + ".h5"
 
 # time params 
-s_datetime = datetime(2020, 10, 18, 0, 0, 0) # start date
-e_datetime = datetime(2020, 11, 4, 0, 0, 0) # end date
-time_step = timedelta(days=0, hours = 1, minutes = 0) # time block size
-
-# has to be a better way than this rubbish... 
-r_datetimes, r_datetimes_f, run_dates, run_datetimes_f, run_datetimes = {}, {}, [], [], []
-t_datetime = s_datetime
-while t_datetime < e_datetime:
-    date_id = t_datetime.date().strftime("%b_%d_%Y")
-    run_dates.append(date_id)
-    run_datetimes_f.append(t_datetime.strftime("%b_%d_%Y_%H_%M"))
-    run_datetimes.append(t_datetime)
-    t_datetime = t_datetime + time_step
-
-# split into lists by date
-r_datetimes_f, r_datetimes = {}, {}
-for date_i in set(run_dates):
-    for date_idx in range(0,len(run_dates)):
-        if date_i == run_dates[date_idx]:
-            if date_i not in r_datetimes:
-                r_datetimes[date_i], r_datetimes_f[date_i] = [], []
-                r_datetimes_f[date_i].append(run_datetimes_f[date_idx])
-                r_datetimes[date_i].append(run_datetimes[date_idx])
-            else:
-                r_datetimes_f[date_i].append(run_datetimes_f[date_idx])
-                r_datetimes[date_i].append(run_datetimes[date_idx])
+s_datetime = datetime(2020, 10, 17, 0, 0, 0) # start date
+e_datetime = datetime(2020, 10, 20, 0, 0, 0) # end date
+num_days = (to_date(e_datetime) - to_date(s_datetime)).days
 
 # go through dates and get the submissions, comments then count tickers
 all_tickers = {}
 
-sorteddates = sorted(r_datetimes.items(), key=lambda x: x[1])
-for i, (date_id, day_dates) in enumerate(sorteddates):
-    print("\nCounting subreddit: " + subreddit_name + " for day " + date_id, "(" + str(i+1) + " of", str(len(r_datetimes.items())) + ")" )
-    day_tickers = []
-    time_point_1 = int(day_dates[0].timestamp())
-    while i < len(sorteddates) and time_point_1 < sorteddates[i+1][1][0].timestamp():
+# current time is the 'After' time 
+current_time = int(s_datetime.timestamp())
 
-        # url for time points
-        sid_url = "https://api.pushshift.io/reddit/search/submission/?subreddit=" + subreddit_name + "&sort=asc&sort_type=created_utc&after=" + str(time_point_1) + "&fields=id,created_utc,num_comments&size=1000"# + "&before="+str(time_point_2) + "&size=1000"
-        print('\n\nGetting submission IDs from ', datetime.fromtimestamp(time_point_1))#, ' - ', datetime.fromtimestamp(time_point_2))
+# initialise the ticker counter for this time point
+tp_num_comments = 0
+day_counter = 0
+tp_tickers = {} 
+day_start_time = time.time()
+    
+while current_time < int(e_datetime.timestamp()):
+    
+    print("\nCounting subreddit: " + subreddit_name + " for day " + to_time_srt(current_time) +  " (" + str(day_counter+1) + " of", str(num_days) + ")" )
 
-        # get request
-        r = requests.get(sid_url)
-        try:
-            data = json.loads(r.text)
-            tp_num_comments, tp_tickers, sub_time = 0, {},  []
+    # url for time points
+    sid_url = get_pushshift_url(subreddit_name, current_time)
+    print('\n\nGetting submission IDs from', to_date_str(current_time)) #, ' - ', datetime.fromtimestamp(time_point_2))
 
-            num_sub_beween_tp = len(data['data'])
-            # time.sleep(1)
-            print('Processing submissions and comments...')
-            for sub in data['data']:
+    # get request
+    r = requests.get(sid_url)
 
-                if sub['num_comments'] == 0:
-                    # skip submissions without comments.
-                    continue
-
-                # FIXME
-                # if sub.created_utc is next day:
-                #    break
-
+    try:
+        data = json.loads(r.text)
+        num_sub_beween_tp = len(data['data'])
+        print('Processing submissions and comments...')
+        for sub in data['data']:
+            if sub['num_comments'] == 0:
+                # skip submissions without comments.
+                continue
+            
+            if is_current_day(current_time, sub['created_utc']):
                 # get submission from praw
                 sid = sub['id']
-                time_point_1 = int(sub['created_utc']) + 1
-
+                current_time = int(sub['created_utc']) + 1
+                #print(current_time,to_date_str(current_time))
+                
                 submission = reddit.submission(id=sid)
-                sub_time = submission.created_utc
                 submission.comment_sort = 'new'
-
-
                 # skip submission if there are a massive amount of comments
                 if submission.num_comments > 0 and submission.num_comments < 15000 :
                     submission.comments.replace_more(limit=comment_limit)
@@ -138,19 +139,28 @@ for i, (date_id, day_dates) in enumerate(sorteddates):
                                     tp_tickers[word] += 1
                                 else:
                                     tp_tickers[word] = 1
-        except:
-            print('Skipping.....')
-            continue
+                                    
+            else:
+                #print("HERE - this is the next day DAYDAY")
+                print("Found {s} submissions with {c} comments ".format(s=num_sub_beween_tp, c = tp_num_comments))
+                print("Time taken: " + str(round(int(time.time() - day_start_time) / 60)) + " minutes")
+                time.sleep(5)
+                print(to_date_str(current_time),to_date_str(sub['created_utc']))
+                all_tickers[to_date_str(current_time)] = tp_tickers # merge_timepoints(tp_tickers)
+                day_counter+=1
+                
+                # write out day
+                try:
+                    write_day(date_id = to_date_str(current_time), ticker_names = [*tp_tickers.keys()], count_vals = [*tp_tickers.values()], out_file = out_file)
+                except:
+                    print("Failed during write out for " + date_id)
+                
+                # reset ticker counter for next day
+                current_time = int(sub['created_utc']) + 1
+                tp_num_comments, tp_tickers = 0, {}
 
-        print("Found {s} submissions with {c} comments ".format( s=num_sub_beween_tp, c = tp_num_comments))
-        res = dict(sorted(tp_tickers.items(), key=lambda item: item[1], reverse = True))
-        print(res)
-        day_tickers.append(res)
-    
-    # merge all over the day and then write the day
-    all_tickers[date_id] = merge_timepoints(day_tickers)
-    try:
-        write_day(date_id = date_id, ticker_names = [*all_tickers[date_id].keys()], count_vals = [*all_tickers[date_id].values()], out_file = out_file)
     except:
-        print("Failed during write out for " + date_id)
+        print('Skipping.....')
+        continue
+
 print("Done.")
