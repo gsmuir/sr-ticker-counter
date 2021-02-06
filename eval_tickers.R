9c03d46b04954364f435564f0b5401c65652437a
gareth muirhead <gsmuirhead@gmail.com>
2021-02-06 22:08:30 +0000

    transforms  and basic plot

--- eval_tickers.R
+++ eval_tickers.R
@@ -10,7 +10,10 @@
 library(rhdf5)
 library(anytime)
 library(plyr)
+
+library(FC14.plotting.lib)
 library(quantmod)
+library(latticeExtra)
 
 ### FUNCTIONS #################################################################
 get.timepoints <- function(h5file) {
@@ -25,6 +28,10 @@ get.timepoints <- function(h5file) {
     Date = dates_format,
     stringsAsFactors = F
   );
+  
+  # order by date
+  ts.data <- ts.data[ order(ts.data$Date, decreasing = F) ,]
+  ts.data$DateID <- make.names(ts.data$Date)
   return(ts.data)
 } 
 
@@ -56,8 +63,64 @@ merge.timepoints <- function(x) {
   return(tickers.merged)
 }
 
+
+to.strdate <- function(x) {
+  time.points[match(x, time.points$DateID),"ID"]
+}
+
+get.merged.data <- function(ticker.i, transform = "none"){
+  stock.price <- get.stock.price(ticker.i,  min.date, max.date)
+  stock.count <- get.ticker.count(ticker.i) # min max date for stock counts
+  
+  merged.count <- as.data.frame(
+    cbind(Count = stock.count, stock.price[match(names(stock.count), rownames(stock.price)),])
+  );
+  
+  merged.count$Ticker = ticker.i
+  merged.count$Date = rownames(merged.count)
+  colnames(merged.count) <- gsub(paste0(ticker.i,"."), "", colnames(merged.count))
+  
+  # transform 
+  if (transform == "log") {
+    merged.count$Count <- log2(merged.count$Count + 1)
+  } else if (transform == "sqrt") {
+    merged.count$Count <- sqrt(merged.count$Count)
+  } else if (transform == "rollMean") {
+    merged.count$Count <- rollmean(log2(merged.count$Count + 1), k = 3, fill = NA)
+  }
+  merged.count$Transform = transform
+  return(merged.count)
+}
+
+get.ticker.count <-  function(ticker.i) {
+  stock.count <- merged.timepoints[ticker.i,]
+  return(stock.count)
+}
+
+get.stock.price <- function(stock, from.date, to.date) {
+  
+  # TODO ticker prices
+  soi <- as.data.frame(
+    getSymbols(
+      stock, 
+      from = from.date,
+      to = to.date, 
+      warnings = FALSE,
+      auto.assign = F
+    ))
+  
+  tmp.date <- rownames(soi)
+  tmp.DateID <- make.names(tmp.date)
+  
+  rownames(soi) <- time.points[match( tmp.DateID, time.points$DateID),'ID']
+  soi$ID  == time.points[match( tmp.DateID, time.points$DateID),'ID']  
+  
+  return(soi)
+}
+
 ### MAIN ######################################################################
-h5file <- "out/out_tickers_stocks.h5"
+
+h5file <- "out/out_tickers_wallstreetbets.h5"
 time.points <- get.timepoints(h5file)
 min.date = min(time.points$Date)
 max.date = max(time.points$Date)
@@ -71,30 +134,44 @@ merged.timepoints <- merge.timepoints(tps.ticker.data)
 merged.timepoints <- merged.timepoints[rowSums(merged.timepoints) > 5,]
 
 # get the ranksum of each ticker and order
-merged.timepoints.ranks <- sapply(1:ncol(merged.timepoints), function(x) rank(desc(merged.timepoints[,x])))
-dimnames(merged.timepoints.ranks) <- dimnames(merged.timepoints)
-plot(ticker.day.ranks['GME',])
-merged.timepoints['GM',]
-merged.timepoints.ranks['GME',]
-
-# ranksum
-ticker.ranks <- sort(
-  setNames(rowSums(merged.timepoints.ranks), rownames(merged.timepoints)), decreasing = F
-  );
-
-ticker.day.ranks['PLTR',]
-
-merged.timepoints.ranks.sorted <- merged.timepoints.ranks[names(ticker.ranks),]
-merged.timepoints.ranks.sorted[1:15,]
+#merged.timepoints.ranks <- sapply(1:ncol(merged.timepoints), function(x) rank(desc(merged.timepoints[,x])))
+#dimnames(merged.timepoints.ranks) <- dimnames(merged.timepoints)
 
 
-plot( 1:num.timepoints, merged.timepoints.ranks.sorted['GME', ])
 
-# TODO ticker prices
-getSymbols(
-  "GME", 
-  from = min.date,
-  to = max.date, 
-  warnings = FALSE,
-  auto.assign = TRUE
-  );
+merge.plot <- function(dat){
+  
+  # x axis scale 
+  dat$dateNumeric <- 1:nrow(dat)
+  
+  if (unique(dat$Transform) == "log") {
+    yax <- expression(paste('Count', ' (log'[2],")" ))
+  } else if (unique(dat$Transform) == "sqrt") {
+    yax <- unique(dat$Transform) 
+  } else if (unique(dat$Transform) == "rollMean") {
+    yax <- "rolling avg (k=3)"
+  }
+
+  cnt.plot <- xyplot(
+    Count ~ dateNumeric, dat, type = "l" , lwd = 3,
+    ylab = list(yax, fontsize = 12),
+    ylab.right = list("Price ($)", fontsize = 12),
+    main = unique(dat$Ticker))
+  
+  stock.plot <- xyplot(Adjusted ~ dateNumeric, dat, type = "s", lwd = 3 )
+  doubleYScale(cnt.plot, stock.plot)
+}
+
+
+ticker.i <- "TSLA"
+select.ticker.dat <- get.merged.data(ticker.i, transform = "rollMean")
+merge.plot(select.ticker.dat)
+
+
+
+select.ticker.dat <- get.merged.data(ticker.i, transform = "log")
+merge.plot(select.ticker.dat)
+
+
+
+
